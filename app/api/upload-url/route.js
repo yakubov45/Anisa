bee3e296930ad4d8c4a900708977b8f17d044744
Muhammad <muhammadyoqubjonov7@gmail.com>
2026-07-11 09:31:31 +0500

    Fix R2 SSL upload error

--- app/api/upload-url/route.js
+++ app/api/upload-url/route.js
@@ -9,6 +9,7 @@ const s3Client = new S3Client({
         accessKeyId: process.env.R2_ACCESS_KEY_ID || "",
         secretAccessKey: process.env.R2_SECRET_ACCESS_KEY || "",
     },
+    forcePathStyle: true,
 });
 
 export async function POST(req) {
