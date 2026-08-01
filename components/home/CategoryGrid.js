28f9c17a2744c95e2cbd8a8450c06e68afb7b1b0
Muhammad <muhammadyoqubjonov7@gmail.com>
2026-08-01 23:16:31 +0500

    errorlarni to'g'irlash

--- components/home/CategoryGrid.js
+++ components/home/CategoryGrid.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 import Link from 'next/link';
+import { useTranslation } from "@/lib/LanguageContext";
 import { getCategoryDisplayName } from "@/lib/constants";
 
 const categories = [
